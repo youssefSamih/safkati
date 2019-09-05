dad703f0163701135452802522f4452a2e776bd7
abderrahim cloudme <abderrahim.cloudme@gmail.com>
2019-09-05 12:50:35 +0100

    setup AppLoading

--- App.js
+++ App.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import { AppLoading, Asset } from 'expo';
+
+import Navigation from './navigation';
+import { Block } from './components';
+
+const images = [];
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  }
+
+  handleResourcesAsync = async () => {
+    // we're caching all the images
+    // for better performance on the app
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+
+    return Promise.all(cacheImages);
+  }
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this.handleResourcesAsync}
+          onError={error => console.warn(error)}
+          onFinish={() => this.setState({ isLoadingComplete: true })}
+        />
+      )
+    }
+
+    return (
+      <Block white>
+        <Navigation />
+      </Block>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
