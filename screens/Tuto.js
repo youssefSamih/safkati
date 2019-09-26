b7fa8f00a6badbb5da5af0a87d42661f760bb573
abderrahim cloudme <abderrahim.cloudme@gmail.com>
2019-09-26 10:51:14 +0100

    Add video and dummy text into the comment ca marche page

--- screens/Tuto.js
+++ screens/Tuto.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, WebView, Dimensions} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+import { Video } from 'expo-av';
+const { width, height } = Dimensions.get('window');
+
 import { 
 	Container, 
 	Header, 
@@ -28,6 +31,17 @@ class Tuto extends React.Component {
 				/>
 	    ),
 	  });
+	constructor(props){
+		super(props);
+		state = {
+	      isReady: false,
+	      status: null,
+	      quality: null,
+	      error: null,
+	      isPlaying: true
+	    }
+
+	}
 	render(){
 		return( 
 			<Container>
@@ -42,7 +56,14 @@ class Tuto extends React.Component {
 				  </Body>
 				</Header>				
 				<Content>
-				 	<Enconstruction />
+				 <WebView
+			        style={{ width, height: 300 }}
+			        javaScriptEnabled={true}
+			        source={{uri: 'https://www.youtube.com/embed/mSwtx7wFDPY?rel=0&autoplay=0&showinfo=0&controls=0'}}
+				/>
+				<Text style={{padding:10}}>
+				Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+				</Text>
 				</Content>
 			</Container>
 		);
